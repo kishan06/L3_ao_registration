b3ac18edc727615c06100ab9b85f46f01b608aec
kishan06 <kishan.bhuta1998@gmail.com>
2025-03-31 13:38:27 +0530

    navigation via go router added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:l2_a_o_registration/pages/initial_registration_page/initial_registration_page_widget.dart';
 import 'package:l3_gov_portal/core/di/service_locator.dart';
 import 'package:l3_gov_portal/core/navigation/app_navigation_service.dart';
 import 'package:l3_gov_portal/core/navigation/navigation_observer.dart';
 import 'package:l3_gov_portal/core/navigation/router.dart';
 import 'package:l3_gov_portal/utils/logger.dart';
+import 'package:go_router/go_router.dart';
+import 'package:l2_a_o_registration/pages/registration/registration_widget.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,21 +27,31 @@ Future<void> main() async {
 
 class MyGovApp extends StatelessWidget {
   const MyGovApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    final navigatorKey = getIt<AppNavigationService>().navigatorKey;
-    return MaterialApp(
+    final router = GoRouter(
+      routes: [
+        GoRoute(
+          path: '/',
+          builder: (context, state) => const RegistrationWidget(),
+        ),
+        GoRoute(
+          name: 'Initial_registration_page',
+          path: '/initialRegistrationPage',
+          builder: (context, state) => const InitialRegistrationPageWidget(),
+        ),
+        // Add other routes here
+      ],
+    );
+
+    return MaterialApp.router(
       title: 'Ao Registration',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      navigatorKey: navigatorKey,
-      navigatorObservers: [
-        getIt<AppNavigationObserver>(),
-      ],
-      onGenerateRoute: AppRouter.generateRoute,
-      onUnknownRoute: AppRouter.onUnknownRoute,
+      routerConfig: router,
     );
   }
 }
